484413a6c91dc2d3624b75789573aa9ab65d360a
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-03-09 00:42:09 +0600

    double or tripple equal problem

---
+++ double vs tripple equal.js
@@ -0,0 +1,15 @@
+const first = 3;
+// const second = '3';
+const second = true ;
+
+/**
+ * === strictly check or check value and type
+ * == just check the value
+ * true is like 1 
+ * false is like 0 
+ */
+if (first === second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is fasle');
+}
